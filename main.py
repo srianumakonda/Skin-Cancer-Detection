7766d8ada7da02b5b0fa8ea9e1aaeeb0d55cbf2f
Sri Anumakonda <54557955+srianumakonda@users.noreply.github.com>
2021-02-05 15:36:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Activation, Flatten, Dropout
+from tensorflow.keras.layers import Dense, Activation, Flatten
 from tensorflow.keras.optimizers import Adam
+import tensorflow_hub as hub
+
+#pip install tensorflow_hub 
 
 train_path = ".\\data\\train"
 test_path = ".\\data\\test"
-img_size = 128
+img_size = 224
 batch_size = 32
 epochs = 10
 
@@ -41,31 +44,33 @@ fig = plt.figure(figsize=(15,10))
 #     plt.imshow(train_dataset[0][0][i-1])
 # plt.show()
 
+url = "https://tfhub.dev/google/tf2-preview/mobilenet_v2/classification/4"
 
-model = Sequential()
-model.add(Conv2D(32,(3,3),input_shape=(img_size,img_size,3)))
-model.add(Activation("relu"))
-model.add(MaxPooling2D((2,2)))
+download_model = hub.KerasLayer(url,input_shape=(img_size,img_size,3))
 
-model.add(Conv2D(64,(3,3)))
-model.add(Activation("relu"))
-model.add(MaxPooling2D((2,2)))
-
-model.add(Conv2D(64,(3,3)))
-model.add(Activation("relu"))
-model.add(MaxPooling2D((2,2)))
-
-model.add(Dropout(0.5))
-model.add(Flatten())
-model.add(Dense(512))
-model.add(Activation("relu"))
-model.add(Dense(2))
-model.add(Activation("softmax"))
+model = Sequential([
+    download_model,
+    Dense(2),
+    Activation("softmax")
+])
 
 model.compile(optimizer=Adam(1e-3),
               loss="categorical_crossentropy",
               metrics=["accuracy"])
 
-model.fit(train_dataset,
-          batch_size=batch_size,
-          epochs=epochs)
+# print("\n Model summary: ")
+# print(model.summary())
+
+# print("\n Model Training: ")
+# model.fit(train_dataset,
+#           batch_size=batch_size,
+#           epochs=epochs)
+        
+# print("\n Model Evaluation: ")
+# model.evaluate(test_dataset)
+
+# print("\n Model save: ")
+# model.save("model.h5")
+
+load_model = tf.keras.models.load_model("model.h5",custom_objects={"KerasLayer":hub.KerasLayer})
+load_model.evaluate(test_dataset)
